f3d651f226c6283c9b0428b0f16378ea201f7bc3
Sangwon Ryu <bdboy090@naver.com>
2020-12-07 22:37:19 +0900

    Rename LocoClient to AuthClient

--- internal/Account.cs
+++ internal/Account.cs
@@ -12,31 +12,30 @@ namespace LocoDotnet.Internal
         public readonly string osVersion;
         public readonly bool permanent;
         public readonly bool forced;
-        public readonly bool autowithlock;
-        public readonly bool autologin;
 
         public Account(string email, string password, string deviceUUID, string deviceName = "PC", string osVersion = "10.0", bool permanent = false, bool forced = false, bool locked = false)
         {
-            this.email = email;
-            this.password = password;
-
             if (String.IsNullOrEmpty(email) || String.IsNullOrEmpty(password))
             {
                 throw new ArgumentNullException();
             }
-
-            if (!String.IsNullOrEmpty(deviceUUID))
-            {
-                this.deviceUUID = Convert.ToBase64String(Encoding.UTF8.GetBytes(deviceUUID));
-            }
             else
+            {
+                this.email = email;
+                this.password = password;
+            }
+
+            if (String.IsNullOrEmpty(deviceUUID))
             {
                 this.deviceUUID = Convert.ToBase64String(Guid.NewGuid().ToByteArray());
             }
+            else
+            {
+                this.deviceUUID = Convert.ToBase64String(Encoding.UTF8.GetBytes(deviceUUID));
+            }
+
             this.deviceName = deviceName;
             this.osVersion = osVersion;
-            this.autologin = locked ? true : false;
-            this.autowithlock = locked ? true : false;
             this.permanent = permanent;
             this.forced = forced;
         }
